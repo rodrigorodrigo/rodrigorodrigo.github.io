3f24b407e347ee476bab5a8d7833e3a06eadc085
rodrigorodrigo <46758811+rodrigorodrigo@users.noreply.github.com>
2020-04-10 15:11:24 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+(function() {
+  /**
+   * Helper object for working with countries data and extracting information.
+   * See countries-data.js for format of the countries data set.
+   */
+  /**
+   * 
+   *   var quickviews = bulmaQuickview.attach(); // quickviews now contains an array of all Quickview instances
+
+   * Register click handlers for every menu item in the page.  Use the `countries`
+   * and `tableHelper` Objects, and their associated methods, to update/populate
+   * the #table-rows table body with the appropriate set of countries, based on the
+   * menu item clicked.
+   *
+   * Make sure you also update the #subtitle heading to properly reflect what
+   * is in the table after you populate it. For example: "List of Countries
+   * and Dependencies - Population between 1 and 2 million" or "List of Countries
+   * and Dependencies - All countries in Asia" etc.
+   */
+  function setupMenuHandlers() {
+    document.querySelectorAll('.modal-button').forEach(function(el) {
+      el.addEventListener('click', function() {
+        var target = document.querySelector(el.getAttribute('data-target'));
+        target.classList.add('is-active');
+        target.querySelector('.modal-close').addEventListener('click', function() {
+          target.classList.remove('is-active');
+        });
+      });
+    });
+  }
+
+  window.onload = setupMenuHandlers;
+})();

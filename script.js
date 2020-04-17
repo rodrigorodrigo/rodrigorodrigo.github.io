c874c07856025cb6c2748140fe92178df541029e
rodrigorodrigo <46758811+rodrigorodrigo@users.noreply.github.com>
2020-04-17 03:26:55 -0400

    dropdown fixed

--- script.js
+++ script.js
@@ -59,7 +59,79 @@
   });
 
 
+  $(document).ready(function(){
+    $('#select').on('change', function() {
+      switch(this.value){
+        case 'none':
+          $("#airplanes-box").hide();
+          $("#helicopters-box").hide();
+          $("#subscribe-form").hide();
 
+          
+          break;
+        case 'all':
+          $("#airplanes-box").show();
+          $("#helicopters-box").show();
+          $("#subscribe-form").show();
+
+          break;
+        case 'airplanes':
+          $("#airplanes-box").show();
+          $("#helicopters-box").hide();
+          $("#subscribe-form").show();
+
+          break;
+        case 'helicopters':
+          $("#airplanes-box").hide();
+          $("#helicopters-box").show();
+          $("#subscribe-form").show();
+
+          break;
+       }
+    });
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+
+    // Dropdowns
+  
+    var $dropdowns = getAll('.dropdown:not(.is-hoverable)');
+  
+    if ($dropdowns.length > 0) {
+      $dropdowns.forEach(function ($el) {
+        $el.addEventListener('click', function (event) {
+          event.stopPropagation();
+          $el.classList.toggle('is-active');
+        });
+      });
+  
+      document.addEventListener('click', function (event) {
+        closeDropdowns();
+      });
+    }
+  
+    function closeDropdowns() {
+      $dropdowns.forEach(function ($el) {
+        $el.classList.remove('is-active');
+      });
+    }
+  
+    // Close dropdowns if ESC pressed
+    document.addEventListener('keydown', function (event) {
+      var e = event || window.event;
+      if (e.keyCode === 27) {
+        closeDropdowns();
+      }
+    });
+  
+    // Functions
+  
+    function getAll(selector) {
+      return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+    }
+  });
+  
 
 
 

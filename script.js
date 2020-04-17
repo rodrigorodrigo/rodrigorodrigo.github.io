b87fc7e1f1dfbfeeee51dff779c72bf556b5dfdf
rodrigorodrigo <46758811+rodrigorodrigo@users.noreply.github.com>
2020-04-16 21:27:23 -0400

    image resize; content added;

--- script.js
+++ script.js
@@ -34,35 +34,39 @@
 
 
 
+
+
+
   // Close mobile & tablet menu on item click
   $('.navbar-item').each(function(e) {
     $(this).click(function(){
-      if($('#navbar-burger-id').hasClass('is-active')){
-        $('#navbar-burger-id').removeClass('is-active');
+      if($('#navbar-burger').hasClass('is-active')){
+        $('#navbar-burger').removeClass('is-active');
         $('#navbar-menu-id').removeClass('is-active');
       }
     });
   });
 
   // Open or Close mobile & tablet menu
-  $('#navbar-burger-id').click(function () {
-    if($('#navbar-burger-id').hasClass('is-active')){
-      $('#navbar-burger-id').removeClass('is-active');
+  $('#navbar-burger').click(function () {
+    if($('#navbar-burger').hasClass('is-active')){
+      $('#navbar-burger').removeClass('is-active');
       $('#navbar-menu-id').removeClass('is-active');
     }else {
-      $('#navbar-burger-id').addClass('is-active');
+      $('#navbar-burger').addClass('is-active');
       $('#navbar-menu-id').addClass('is-active');
     }
   });
 
 
 
-    
-    
+
+
+
   }
 
 
 
   window.onload = setupMenuHandlers;
-  
+
 })();
